f2cdbd5b481ae300683424e3578e52b9002337a4
muhammad.rifat11 <muhammad.rifat11@ui.ac.id>
2023-12-03 19:56:48 +0700

    rating model for trainer

--- src/main/java/rpl/fitbook/model/pengguna/TrainerModel.java
+++ src/main/java/rpl/fitbook/model/pengguna/TrainerModel.java
@@ -1,17 +1,52 @@
 package rpl.fitbook.model.pengguna;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class TrainerModel extends PenggunaModel {
     private String bio;
     private Float rating;
+    private Float totalRating = 0.0f;
+    private Integer reviewCount = 0;
+
+    // Constructors, getters, and setters
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Float getTotalRating() {
+        return totalRating;
+    }
+
+    public void setTotalRating(Float totalRating) {
+        this.totalRating = totalRating;
+    }
+
+    public Integer getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(Integer reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    // Additional logic for adding ratings
+    public void addRating(Float newRating) {
+        this.totalRating += newRating;
+        this.reviewCount++;
+        this.rating = this.totalRating / this.reviewCount;
+    }
 }
---
+++ src/main/java/rpl/fitbook/service/trainer/TrainerService.java
@@ -0,0 +1,6 @@
+package rpl.fitbook.service.trainer;
+
+public interface TrainerService {
+    void rateTrainer(Long trainerId, Float rating);
+    // Additional methods can be defined here
+}

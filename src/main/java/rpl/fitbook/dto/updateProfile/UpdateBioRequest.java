74c338cfab364d54884fe64e8298886bab1d4180
AlifIsmady <alifismady@gmail.com>
2023-12-17 17:07:17 +0700

    fix updateBio and updateProfile service

---
+++ src/main/java/rpl/fitbook/dto/updateProfile/UpdateBioRequest.java
@@ -0,0 +1,13 @@
+package rpl.fitbook.dto.updateProfile;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateBioRequest {
+    private String newBio;
+}

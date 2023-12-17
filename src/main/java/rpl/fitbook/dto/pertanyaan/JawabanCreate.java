e6919f1f9a69af863dc9b577d88a89b30ea14927
fajarriv <rivaldiibnusina@gmail.com>
2023-12-17 20:52:34 +0700

    remove builder annotation

--- src/main/java/rpl/fitbook/dto/pertanyaan/JawabanCreate.java
+++ src/main/java/rpl/fitbook/dto/pertanyaan/JawabanCreate.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
+// @Builder
 public class JawabanCreate {
     @NotBlank(message = "jawaban is required.")
     private String jawaban;
